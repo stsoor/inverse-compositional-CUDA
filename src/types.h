b59d1f343fc1d92b4dfc1be107959fc9e1cc584a
Bence Sarkadi <stsoor@gmail.com>
2018-02-17 01:02:11 +0100

    types.h got lost somewhere along the way

---
+++ src/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdlib>
+
+struct dimensions
+{
+    std::size_t id;
+    std::size_t position;
+    std::size_t rows;
+    std::size_t cols;
+};
+
+#endif //TYPES_H
